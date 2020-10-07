d4084984d3f8c1ebb02941841d7946b9d5cc51b8
Ethan <donaheth000@hsestudents.org>
2020-10-07 12:10:06 -0400

    front end

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 import Timer from "./timeManager.js";
-import Dropdown from "./dropdown.js"
+import Dropdown from "./dropdown.js";
+import Header from "./header.js";
 
 function App() {
   return (
     <div className="App">
+    <Header />
+    <div className="timeContent">
     <Timer />
     <Dropdown />
+    </div>
     </div>
   );
 }
